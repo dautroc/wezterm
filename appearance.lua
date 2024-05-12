f9b0bb7f4360e3fae21d42e19d64d1b2b7bb5cd0
Loi <loi.nguyen@employmenthero.com>
2024-05-12 23:37:19 +0700

    Update config

--- appearance.lua
+++ appearance.lua
@@ -18,7 +18,7 @@ G.font_size = 13
 -- Tab bar
 G.use_fancy_tab_bar = false
 G.hide_tab_bar_if_only_one_tab = false
-G.enable_tab_bar = false
+G.enable_tab_bar = true
 G.tab_max_width = 50
 G.tab_bar_at_bottom = false
 G.show_tab_index_in_tab_bar = true
